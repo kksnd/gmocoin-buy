107f0c8f8590bf6e786e591fcb990919b5e3dc78
Thousanda <k0k1senda@gmail.com>
2021-05-17 14:52:19 +0900

    small fix

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#DIR=/your/path/to/this/repo
+DIR=/your/path/to/this/repo
 
 LOG=${DIR}/run.log
 
-cd $DIR || exit
+cd "$DIR" || exit
 
-python main.py >> $LOG 2>&1
+python main.py >> "$LOG" 2>&1
